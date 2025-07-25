ca7f1efb0317c7b4c6bc0d9c9ca184e0dafe917e
Vicky Cao <miavickcao@gmail.com>
2025-07-25 13:03:24 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
